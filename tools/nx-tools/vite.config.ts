b3208a4a14a046caa7e4b6adf9d1fc88513caac9
Vincent <vcapic@gmail.Com>
2025-08-29 14:00:05 +0200

    fix lint

--- tools/nx-tools/vite.config.ts
+++ tools/nx-tools/vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
-  root: __dirname,
+  root: import.meta.dirname,
   cacheDir: '../../node_modules/.vite/tools/nx-tools',
   plugins: [],
   // Uncomment this if you are using workers.
